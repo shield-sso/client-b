0e5cf616708d49b0c0f2717fb12101cda590a0f9
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2017-08-11 00:04:41 +0200

    web

--- web/app.php
+++ web/app.php
@@ -1,20 +1,17 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
-if (PHP_VERSION_ID < 70000) {
-    include_once __DIR__.'/../var/bootstrap.php.cache';
+
+if (getenv('APP_ENV') === 'dev') {
+    Debug::enable();
+    $kernel = new AppKernel('dev', true);
+} else {
+    $kernel = new AppKernel('prod', false);
 }
 
-$kernel = new AppKernel('prod', false);
-if (PHP_VERSION_ID < 70000) {
-    $kernel->loadClassCache();
-}
-//$kernel = new AppCache($kernel);
-
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
